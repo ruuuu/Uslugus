cb867357d4e4254d5c0e2822a3a7d24451a6de40
ruuuu <rufinka_91@mail.ru>
2022-09-27 16:59:27 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.html';
+import './index.scss';
+
+
+
+// const imgWrap = document.querySelector('.img');
+// const img = new Image();
+// img.src = code;
+// img.width = 700;
+// imgWrap.append(img);
+
