ccf2561791827e826c55f86a72b559b95b81b726
ruuuu <rufinka_91@mail.ru>
2022-11-07 21:17:52 +0300

    Add files via upload

--- src/modules/const.js
+++ src/modules/const.js
@@ -1,5 +1,5 @@
-export const API_URL = 'https://titanium-spotted-memory.glitch.me'; // адрес лок  сервера   http://localhost:3024
-// апи загрузиkb на  хостинг Glitch и полули о него урл и вставили его https://titanium-spotted-memory.glitch.me
+export const API_URL = 'https://momentous-innate-save.glitch.me'; // адрес лок  сервера   http://localhost:3024
+// апи загрузиkb на  хостинг Glitch и полули о него урл и вставили его https://momentous-innate-save.glitch.me
 
 export const directions = {
       'from': 'От',
