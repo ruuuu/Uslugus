4d61cc30a3cf73b607f73bdd0dccf071305f607c
ruuuu <rufinka_91@mail.ru>
2022-11-07 20:44:13 +0300

    Add files via upload

--- src/modules/const.js
+++ src/modules/const.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://titanium-spotted-memory.glitch.me'; // адрес лок  сервера  http://localhost:3024
-// апи загрузи на  хостинг Glitch и полули о него урл и вставили его
+// апи загрузиkb на  хостинг Glitch и полули о него урл и вставили его
 
 export const directions = {
       'from': 'От',
