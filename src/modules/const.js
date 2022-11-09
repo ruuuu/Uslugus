5df10def0e3bbd5262d73cec01f6de1dbb76e54e
ruuuu <rufinka_91@mail.ru>
2022-11-09 16:17:04 +0300

    Add files via upload

--- src/modules/const.js
+++ src/modules/const.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://momentous-innate-save.glitch.me'; // адрес лок  сервера   http://localhost:3024
+export const API_URL = 'http://localhost:3024'; // адрес лок  сервера   http://localhost:3024
 // апи загрузиkb на  хостинг Glitch и полули о него урл и вставили его https://momentous-innate-save.glitch.me
 
 export const directions = {
